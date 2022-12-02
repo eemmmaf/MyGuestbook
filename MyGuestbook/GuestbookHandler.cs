3320b7cf534b662190b3a26cb024d5157417e150
Emma Forslund <emfo2102@student.miun.se>
2022-12-02 15:57:02 +0100

    First commit

--- MyGuestbook/GuestbookHandler.cs
+++ MyGuestbook/GuestbookHandler.cs
@@ -12,10 +12,10 @@ namespace MyGuestbook
         //----------------- Fields ---------------//
 
         //Filens namn
-        private string filename = @"./guestbook.json";
+        private string filename =  @"guestbook.json";
 
         //Lista
-        private List<Post> posts = new List<Post>();
+        private List<Post> posts = new();
 
 
 
@@ -23,10 +23,12 @@ namespace MyGuestbook
         public GuestbookHandler()
         {
             //Kontroll om filen existerar
-            if (File.Exists(@"./guestbook.json") == true)
+            if (File.Exists( @"guestbook.json") == true)
             { // Läser in filen om filen existerar
                 string jsonString = File.ReadAllText(filename);
-                posts = JsonSerializer.Deserialize<List<Post>>(jsonString)!;
+            
+                    posts = JsonSerializer.Deserialize<List<Post>>(jsonString);
+                
             }
         }
 
@@ -37,7 +39,7 @@ namespace MyGuestbook
         public Post AddPost(Post post)
         {
             posts.Add(post);
-            Marshal();
+            Store();
             return post;
         }
 
@@ -46,17 +48,17 @@ namespace MyGuestbook
         public int DeletePost(int index)
         {
             posts.RemoveAt(index);
-            Marshal();
+            Store();
             return index;
         }
 
         //Returnera alla inlägg
-        public List<Post> getPosts()
+        public List<Post> GetPosts()
         {
             return posts;
         }
 
-        private void Marshal()
+        private void Store()
         {
             // Serialiserar och sparar till filen
             var jsonString = JsonSerializer.Serialize(posts);
--- MyGuestbook/Program.cs
+++ MyGuestbook/Program.cs
@@ -20,77 +20,96 @@ namespace MyGuestbook
             //När programmet startas 
             while (true)
             {
+
                 Console.Clear(); Console.CursorVisible = false;
                 Console.Clear();
-                Console.WriteLine("Emmas Gästbok");
+                Console.WriteLine("++===========Emmas Gästbok===========++");
 
                 Console.WriteLine("\n1. Skriv i gästbok");
                 Console.WriteLine("2. Ta bort inlägg");
-                Console.WriteLine("\nX. Avsluta");
+                Console.WriteLine("\nX. Avsluta\n");
+                Console.WriteLine(" -----------------------");
+                Console.WriteLine(" |                     |");
+                Console.WriteLine(" |    Emmas gästbok    |");
+                Console.WriteLine(" |                     |");
+                Console.WriteLine(" -----------------------\n");
 
                 //Sätter i till 0
                 int i = 0;
                 //Loopar igenom inlägg och skriver ut i konsolen
-                foreach (Post p in guestbook.getPosts())
+                foreach (Post p in guestbook.GetPosts())
                 {
                     Console.WriteLine("[" + i++ + "] " + p.Name + " - " + p.Content);
-
                 }
 
-
-                //----------- Switch-sats--------------//
 
 
                 //Läser in användarens val
                 int input = (int)Console.ReadKey(true).Key;
 
-
+                //----------- Switch-sats--------------//
                 switch (input)
                 {
                     case '1':
+                        Console.Clear();
                         Console.CursorVisible = true;
 
                         //Namn
                         Console.Write("Ange ditt namn: ");
                         var name = Console.ReadLine();
-                        //Kontroll om namnet är tomt
-                        if (string.IsNullOrEmpty(name))
+                        if (String.IsNullOrEmpty(name))
                         {
-                            Console.WriteLine("Namn måste fyllas i. Tryck på valfri knapp för att börja om");
+                            Console.WriteLine("Namn måste fyllas i. Klicka på valfri knapp för att starta om");
+                            Console.ReadKey();
+                            break;
                         }
 
                         //Instansierar klassen Post
                         Post obj = new();
-
-
                         //Kontroll om namn inte är null
                         if (name is not null)
                         {
                             obj.Name = name;
                         }
-                        
+
 
                         Console.Write("Skriv inlägg: ");
                         var content = Console.ReadLine();
-                        if (string.IsNullOrEmpty(content))
+                        if (String.IsNullOrEmpty(content))
                         {
-                            Console.WriteLine("Inlägg måste fyllas i. Tryck på valfri knapp för att börja om");
+                            Console.WriteLine("Innehåll måste fyllas i");
+                            Console.ReadKey();
                         }
+
 
                         //Kontroll om att content inte är null
                         if (content is not null)
                         {
                             obj.Content = content;
                         }
-                        if (!String.IsNullOrEmpty(name)) guestbook.AddPost(obj);
+
+                        //Lägger till om varken namn eller innehåll är tomt
+                        if (!String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(content))
+                        { guestbook.AddPost(obj); }
+
                         break;
+
                     case '2':
+                        Console.Clear();
                         Console.CursorVisible = true;
                         Console.Write("Ange nummer att radera: ");
                         var index = Console.ReadLine();
-                        guestbook.DeletePost(Convert.ToInt32(index));
+                        if (index is not null)
+                        {
+                            guestbook.DeletePost(Convert.ToInt32(index));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ett nummer måste väljas");
+                        }
                         break;
                     case 88:
+                        Console.Clear();
                         Environment.Exit(0);
                         break;
                 }
